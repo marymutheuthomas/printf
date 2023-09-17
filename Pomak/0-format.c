881da2ef4ad0557a853bb6d8524acacb2e7c8427
marymutheuthomas <marymutheu879@gmail.com>
2023-09-17 15:22:04 -0700

    move filess

--- Pomak/0-format.c
+++ Pomak/0-format.c
@@ -50,9 +50,22 @@ int _printf(const char *format, ...)
 }
 int main()
 {
-	_printf("Mary\n");
-	_printf("%c\n", 'm');
-	_printf("%s\n", "pomak is a string");
-	_printf("%%\n");
+	int len;
+    int len2;
+
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+    len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
+    _printf("Len:[%d]\n", len);
+    printf("Len:[%d]\n", len2);
+    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
 	return 0;
 }
