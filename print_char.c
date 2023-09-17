f02108e2331729b626b8772df5a6f91c678d58df
pomzy001 <dpr4science@gmail.com>
2023-09-17 16:18:18 -0700

    update

---
+++ print_char.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+* print_char - function to print character
+* @c: char to print
+* Return: (c)
+*/
+
+int print_char(int c)
+{
+    if (c)
+        return putchar(c);
+    else
+        return (0);
+}
