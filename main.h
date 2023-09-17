28f36460703c0a6a4a368cd7418a99ace6f68bbc
marymutheuthomas <marymutheu879@gmail.com>
2023-09-17 15:59:51 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include<stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int print_string(char *str);
+int print_char(int c);
+int print_int(int num);
+
+#endif // MAIN_H
+
