f7ae2dc0dff54ac2e5e24d1403ccd7cabef76826
pomzy001 <dpr4science@gmail.com>
2023-09-19 14:56:17 -0700

    updated

--- buffer.c
+++ buffer.c
@@ -1,18 +1,33 @@
 #include "main.h"
 
-/** Flush the buffer */
-void flush_buffer(char *buffer, int *buffer_index) {
-    if (*buffer_index > 0) {
-        write(STDOUT_FILENO, buffer, *buffer_index);
-        *buffer_index = 0;
-    }
+/**
+ * flush_buffer - Flush the buffer
+ *@buffer: buffer
+ *@buffer_index: buffer_index
+ * Return: 0
+ **/
+void flush_buffer(char *buffer, int *buffer_index)
+{
+	if (*buffer_index > 0)
+	{
+	write(STDOUT_FILENO, buffer, *buffer_index);
+	*buffer_index = 0;
+	}
 }
 
-/** Append a character to the buffer */
-void append_to_buffer(char *buffer, int *buffer_index, char c) {
-    if (*buffer_index >= BUFFER_SIZE - 1) {
-        flush_buffer(buffer, buffer_index);
-    }
-    buffer[(*buffer_index)++] = c;
+/**
+ * append_to_buffer - Append a character to the buffer
+ *@buffer: buffer
+ *@buffer_index: buffer_index
+ *@c: char c
+ * Return: 0
+ **/
+void append_to_buffer(char *buffer, int *buffer_index, char c)
+{
+	if (*buffer_index >= BUFFER_SIZE - 1)
+	{
+		flush_buffer(buffer, buffer_index);
+	}
+	buffer[(*buffer_index)++] = c;
 }
 
